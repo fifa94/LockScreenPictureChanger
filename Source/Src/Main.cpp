e48d1631cc729fcb90f7fe1144492f60a984f6d4
Filip Sramek <sramek.filip@gmail.com>
2023-03-27 20:59:00 +0200

    First working reading registry plus writing

--- Source/Src/Main.cpp
+++ Source/Src/Main.cpp
@@ -1,28 +1,38 @@
 #include <iostream>
 #include "MainInclude.hpp"
 #include <string>
+#include <sstream>
 
 int main()
 {
     HKEY hKey;
-
     // Initializing path to the registry
-    std::string str_path = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Privacy";
+    std::string str_path = "SOFTWARE\\Microsoft\\AuthCookies\\Live\\Default\\CAW";
     // String conversion to LPCSTR
     LPCSTR StringPath = str_path.c_str();
 
-    RegistryPictureChange RegPicObj(&hKey, StringPath);
-
-    std::string NameOfRegistry = "PrivacyConsentPresentationVersion";
+    RegistryPictureChange RegPicObj(&hKey, StringPath, REG_SZ);
+    std::string NameOfRegistry = "Test";
 
     LPCSTR StringNameOfRegistry = NameOfRegistry.c_str();
 
-    int valueOfRegistry = 0;
+    //auto valueOfRegistry = 0;
+    size_t size = 256;
+    char buffer[size];
 
-    if (RegPicObj.CheckRegistry(valueOfRegistry, StringNameOfRegistry) != 0)
+    LPSTR Lp_buffer = buffer;
+    
+    long result = RegPicObj.CheckRegistry(Lp_buffer, StringNameOfRegistry);
+
+    if (result != 0)
     {
-        std::cout<< "Error with code: " <<RegPicObj.CheckRegistry(valueOfRegistry, StringNameOfRegistry) <<std::endl;
+        std::cout<< "Error with code: " <<result <<std::endl;
+        return 0;
     }
 
+    LPCSTR lpData = "https://login.live1.com";
+
+    long WriteResult = RegPicObj.WriteRegistry( lpData, StringNameOfRegistry);
+    
     return 0;
 }
--- Source/Src/MainInclude.hpp
+++ Source/Src/MainInclude.hpp
@@ -4,6 +4,7 @@
 #include "windows.h"
 #include <string>
 
+
 #define RegOpenKeyEx __AW_SUFFIXED__(RegOpenKeyEx)
 
 namespace AuxiliaryFunctions {
@@ -23,15 +24,18 @@ private:
     HKEY* _hKey;
     LPCSTR _Path;
     DWORD _dwType, _dwSize;
+    void CloseKey(HKEY* hKey){RegCloseKey(*_hKey);};
+    
 public:
-    RegistryPictureChange(HKEY* hKey, LPCSTR Path);
+    RegistryPictureChange(HKEY* hKey, LPCSTR Path, DWORD dwType);
     ~RegistryPictureChange();
-    long CheckRegistry(int ValueOfRegistry, LPCSTR NameOfRegistry);
+    long CheckRegistry(LPSTR ValueOfRegistry, LPCSTR NameOfRegistry);
+    long WriteRegistry(LPCSTR ValueToWrite,LPCSTR NameOfRegistry);
 };
 
 // Constructor
-RegistryPictureChange::RegistryPictureChange(HKEY* hKey, LPCSTR Path)
-:_hKey(hKey), _Path(Path)
+RegistryPictureChange::RegistryPictureChange(HKEY* hKey, LPCSTR Path, DWORD dwType)
+:_hKey(hKey), _Path(Path), _dwType(dwType)
 {
 }
 
@@ -40,29 +44,47 @@ RegistryPictureChange::~RegistryPictureChange()
 {
 }
 
-long RegistryPictureChange::CheckRegistry(int ValueOfRegistry, LPCSTR NameOfRegistry)
+long RegistryPictureChange::CheckRegistry(LPSTR ValueOfRegistry, LPCSTR NameOfRegistry)
 {
     long result;
 
     if (RegOpenKeyExA(HKEY_CURRENT_USER, _Path, 0, KEY_READ, _hKey) == ERROR_SUCCESS)
     {
-       if (RegQueryValueExA(*_hKey, NameOfRegistry, NULL, &_dwType, (BYTE*)&ValueOfRegistry, &_dwSize) == ERROR_SUCCESS)
+       if (RegQueryValueExA(*_hKey, NameOfRegistry, NULL, &_dwType, (LPBYTE)ValueOfRegistry, &_dwSize) == ERROR_SUCCESS)
         {
             std::cout<< "Value of registry: " << ValueOfRegistry << std::endl;
+            //std::cout<< "V " << _dwSize << std::endl;
 
             result = 0;
         }
         else
         {
-            result = RegQueryValueExA(*_hKey, NameOfRegistry, NULL, &_dwType, (BYTE*)&ValueOfRegistry, &_dwSize);
+            result = RegQueryValueExA(*_hKey, NameOfRegistry, NULL, &_dwType, (LPBYTE)&ValueOfRegistry, &_dwSize);
+            CloseKey(_hKey);
         }
     }
     else
     {
         result = RegOpenKeyExA(HKEY_CURRENT_USER, _Path, 0, KEY_READ, _hKey);
+        CloseKey(_hKey);
     }
 
     return result; 
 
 }
 
+long RegistryPictureChange::WriteRegistry(LPCSTR ValueToWrite,LPCSTR NameOfRegistry)
+{
+    long Result;   
+
+    Result = RegSetValueExA(*_hKey, NameOfRegistry, 0, REG_SZ, (const BYTE*)ValueToWrite, strlen(ValueToWrite) + 1); 
+
+    if (Result != ERROR_SUCCESS) {
+        printf("Error writing registry value: %d\n", Result);
+        CloseKey(_hKey);
+        return Result;
+    }
+
+    CloseKey(_hKey);
+    return Result;
+}
